8040ab90ef602c91b186f04a4af0eb1f70b43484
yarongold <yarongoldstein10@gmail.com>
2023-01-01 18:37:38 +0200

    Update app.py add function to calculate quarters

--- src/app.py
+++ src/app.py
@@ -1,12 +1,25 @@
+import http
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl, requests, json
+import pandas as pd
+
+
+def get_start_end_q():
+    now = pd.Timestamp.now()
+    current_period = now.to_period('Q')
+    next_quarter_period = current_period + 1
+    return next_quarter_period.start_time.date().__str__(), next_quarter_period.end_time.date().__str__()
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def get_data(self):
+        start_q, end_q = get_start_end_q()
         myrequest = requests.get(
-            "https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&start=2021-03-01&end=2021-06-31&ss=on&mf=on&c=on&geo=geoname&geonameid=281184&M=on&s=on")
+            f"https://www.hebcal.com/hebcal"
+            f"?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&"
+            f"start={start_q}&end={end_q}&ss=on&mf=on&c=on&"
+            f"geo=geoname&geonameid=281184&M=on&s=on")
         json_str = myrequest.text
         data = json.loads(json_str)
         json_tmp = []
@@ -14,9 +27,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             title = item['title']
             date = item['date']
             json_tmp.append({"Holiday": title, "Date": date})
-        
+
         json_object = json.dumps(json_tmp, indent=4)
-        print(json_object) 
+        print(json_object)
         return json_object
 
     def do_GET(self):
@@ -24,21 +37,18 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         ret = self.get_data()
         self.wfile.write(ret.encode(encoding='utf_8'))
-    
-    
+
+
 try:
-    httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(('localhost', 8888), SimpleHTTPRequestHandler)
 
-
-    
     #httpd.socket = ssl.wrap_socket(httpd.socket,
     #                               keyfile="key.pem",
     #                               certfile='cert.pem', server_side=True)
-    print('Starting the web server...')
-    httpd.serve_forever()
 
+    httpd.serve_forever()
+    print(get_start_end_q())
 
 except KeyboardInterrupt:
     print('^C received, shutting down the web server')
     httpd.socket.close()
-
