068e48934bc6c1c8e79c955809957dccdf87e465
yarongold <yarongoldstein10@gmail.com>
2023-01-12 08:37:59 +0200

    Update provision.sh
    create /etc/nginx/ssl dir for certificates
    copy self signed certificates to /etc/nginx/ssl
    open ports for nginx
    copy nginx.conf with reverse proxy and ssl to /etc/nginx/nginx.conf
    start nginx service
    make nginx service available at reboot

--- provision.sh
+++ provision.sh
@@ -5,5 +5,10 @@ yum install -y python3 python3-request* python3-http*
 pip3 install --user --upgrade pip
 pip3 install --user setuptools_rust pyopenssl http1 requests pandas
 sudo mkdir -p /etc/nginx/sites-enabled
-sudo cp /vagrant/default /etc/nginx/sites-enabled/default
+sudo mkdir -p /etc/nginx/ssl
+sudo cp /vagrant/ssl/* /etc/nginx/ssl
+sudo setsebool -P httpd_can_network_connect 1
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+sudo cp /srv/src/nginx.conf /etc/nginx/nginx.conf
 service nginx start
+systemctl enable nginx
